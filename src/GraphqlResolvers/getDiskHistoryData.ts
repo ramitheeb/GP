ffe53ed1f4cc1648247c96a1bea85f2dfa55a5dc
Ibrahim Masri <msri.ibraheem@gmail.com>
2021-03-03 22:25:34 +0200

    working on history graphs

--- src/GraphqlResolvers/getDiskHistoryData.ts
+++ src/GraphqlResolvers/getDiskHistoryData.ts
@@ -1,30 +1,38 @@
-import {Aggregation, RedisTimeSeriesFactory, TimestampRange} from "redis-time-series-ts";
+import { Aggregation, RedisTimeSeriesFactory, TimestampRange } from "redis-time-series-ts";
 
-const getDiskHistoryData = async(_,args,context)=>{
-    if(!context.reqx.username) return;
+const getDiskHistoryData = async (_, args, context) => {
+    if (!context.reqx.username) return;
     const factory = new RedisTimeSeriesFactory();
     const client = factory.create();
-    console.log("here");
-    
-    const samples = await client.range("disk-usage:read:medium",new TimestampRange(args.fromDate,args.toDate),undefined,new Aggregation("AVG",args.timeBucket));
-    
+    var sampleFactor: number;
+    switch (args.option) {
+        case "Day":
+            sampleFactor = 150 / 7 * 30;
+            break;
+
+        default:
+            break;
+    }
+
+    const samples = await client.range("disk-usage:read:medium", new TimestampRange(args.fromDate, args.toDate), undefined, new Aggregation("AVG", args.timeBucket));
+
     const data = [{}];
     for (let i = 0; i < samples.length; i++) {
         const element = samples[i];
         data[i] = {
-            rio:element.getValue(),
-            wio:null,
+            rio: element.getValue(),
+            wio: null,
             tIO: null,
             rIO_sec: null,
             wIO_sec: null,
             tIO_sec: null,
             ms: null,
-        }    
+        }
     }
 
     return {
-        fromDate:args.fromDate,
-        toDate:args.toDate,
+        fromDate: args.fromDate,
+        toDate: args.toDate,
         data: data,
     }
 
