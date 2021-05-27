6366fcd93dfd0a7acdd2239db4d24ac8ac74da4f
rami <rami.theeb00@gmail.com>
2021-05-28 00:24:07 +0300

    add mailing

--- src/index.ts
+++ src/index.ts
@@ -103,7 +103,8 @@ var whitelist = [
 
 var corsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) callback(null, true);
+    //   if (whitelist.indexOf(origin) !== -1)
+    callback(null, true);
   },
   credentials: true,
 };
